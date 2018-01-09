4b1b4121e675edc694e7ffff476263160334cab3
Keisuke KITA <kei.kita2501@gmail.com>
2018-01-09 13:12:38 +0900

    http → https

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -4,9 +4,9 @@
         <h1><a href="https://www.kitak.info/">kitak.info</a></h1>
 
         <div id="follow-icons">
-            <a href="http://facebook.com/kei.kita2501" rel="me"><i class="fa fa-facebook-square fa-2x"></i></a>&nbsp;
-            <a href="http://twitter.com/kitak" rel="me"><i class="fa fa-twitter-square fa-2x"></i></a>&nbsp;
-            <a href="http://github.com/kitak" rel="me"><i class="fa fa-github-square fa-2x"></i></a>
+            <a href="https://facebook.com/kei.kita2501" rel="me"><i class="fa fa-facebook-square fa-2x"></i></a>&nbsp;
+            <a href="https://twitter.com/kitak" rel="me"><i class="fa fa-twitter-square fa-2x"></i></a>&nbsp;
+            <a href="https://github.com/kitak" rel="me"><i class="fa fa-github-square fa-2x"></i></a>
         </div>
 
         <h5><a href="https://www.kitak.info/about">About</a></h5>
