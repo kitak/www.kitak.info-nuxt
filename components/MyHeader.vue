dc7b44df995239526efa8ad76f6e9dfe36291bed
Keisuke KITA <kei.kita2501@gmail.com>
2018-01-10 12:43:22 +0900

    ナビゲーションしない場合がある。相対パスに切り替えてみる

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header id="header">
-        <a id="logo" href="https://www.kitak.info/"><img src="https://avatars1.githubusercontent.com/u/228410?v=3&s=460" alt="www.kitak.info" /></a>
-        <h1><a href="https://www.kitak.info/">kitak.info</a></h1>
+        <a id="logo" href="/"><img src="https://avatars1.githubusercontent.com/u/228410?v=3&s=460" alt="www.kitak.info" /></a>
+        <h1><a href="/">kitak.info</a></h1>
 
         <div id="follow-icons">
             <a href="https://facebook.com/kei.kita2501" rel="me"><i class="fa fa-facebook-square fa-2x"></i></a>&nbsp;
@@ -9,7 +9,7 @@
             <a href="https://github.com/kitak" rel="me"><i class="fa fa-github-square fa-2x"></i></a>
         </div>
 
-        <h5><a href="https://www.kitak.info/about">About</a></h5>
-        <h5><a href="https://www.kitak.info/resume">Resume</a></h5>
+        <h5><a href="/about">About</a></h5>
+        <h5><a href="/resume">Resume</a></h5>
     </header>
 </template>
